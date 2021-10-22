c11bc50970c84e54eb5972675bb46642609465d3
ZèD <imzihad@gmail.com>
2021-10-22 23:44:06 +0600

    Initial Router Setup
    Signed-off-by: ZèD <imzihad@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Header from './Pages/Home/Header/Header';
+import Home from './Pages/Home/Home/Home';
+import Product from './Pages/Products/Product/Product';
+import Products from './Pages/Products/Products/Products';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header/>
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/products" component={Products} exact />
+          <Route path="/product/:productID" component={Product} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
